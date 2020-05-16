76e2ff875ae0cc6052554de4e68193299e726914
brendanberg01 <brendan_berg@aol.com>
2020-05-16 17:09:00 +0200

    Add basic led blink code
    A endless loop was used to let the Arduino Uno pin 13 led blink at 2Hz
    to test the build and flash system setup.

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    DDRB |= (1 << DDB5);
+
+    while (true)
+    {
+        PORTB ^= (1 << PORTB5);
+        _delay_ms(500);
+    }
+
     return 0;
 }
